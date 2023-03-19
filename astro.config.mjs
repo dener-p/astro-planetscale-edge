503f96d89a5b18102a6290c2556380c0e9c466c7
Dener <denerpuhl@gmail.com>
2023-03-18 22:38:58 -0300

    changed to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
-import netlify from '@astrojs/netlify/edge-functions';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: netlify({})
+  output: 'static',
 });
