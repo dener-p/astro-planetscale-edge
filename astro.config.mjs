f5b08ea051cbfeac9a151c7696c904577af76720
Dener <denerpuhl@gmail.com>
2023-03-18 21:10:05 -0300

    fixed deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
-import netlify from '@astrojs/netlify/edge-functions';
 // https://astro.build/config
+import vercel from '@astrojs/vercel/edge';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify()
+  adapter: vercel()
 });
