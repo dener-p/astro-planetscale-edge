37d7892fc302d5209215e7db0aacf8c4e559c0fb
Dener <denerpuhl@gmail.com>
2023-03-18 22:40:13 -0300

    changed to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify/edge-functions';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
+  output: 'server',
+  adapter: netlify()
 });
